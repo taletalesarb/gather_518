2e23c47f7403f29c7be4822e969884491a97a5ec
ming <thanawit321@hotmail.com>
2022-11-09 22:14:06 +0700

    add tp for admin

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -45,6 +45,17 @@ function inject()
         return playersOnServer;
     }
 
+    function teleportToMe(targetName) 
+    {
+        let playersOnServer = getPlayers();
+        
+        let x = gameSpace.gameState[gameSpace.id].x;
+        let y = gameSpace.gameState[gameSpace.id].y;
+        let m = gameSpace.gameState[gameSpace.id].map;
+        if (playersOnServer[targetName] != undefined){  
+            game.teleport(m, x, y, playersOnServer[targetName]);
+        }
+    }
 
     function getOverHere(targetName) 
     {
@@ -53,21 +64,21 @@ function inject()
         let playersOnServer = getPlayers();
         let x = gameSpace.gameState[gameSpace.id].x;
         let y = gameSpace.gameState[gameSpace.id].y;
-        let m = gameSpace.gameState[gameSpace.id].map
+        let m = gameSpace.gameState[gameSpace.id].map;
 
         if (playersOnServer[targetName] != undefined) {
             console.log(targetName + " was located with the ID:" + playersOnServer[targetName]);
             
             let px = game.players[playersOnServer[targetName]].x;
             let py = game.players[playersOnServer[targetName]].y;
-            let pm = game.players[playersOnServer[targetName]].map
+            let pm = game.players[playersOnServer[targetName]].map;
             if (px<2){
-                px = px + 2
+                px = px + 2;
             }
             else{
-                px = px - 2
+                px = px - 2;
             }
-            game.teleport(pm, px, py)
+            game.teleport(pm, px, py);
 
             game.enterWhisper(playersOnServer[targetName]);
 
@@ -103,6 +114,8 @@ function inject()
                     clearInterval(tp);
                 }
             }, 200);
+            
+            game.setFollowTarget('');
         } else {
             echo("Player not found");
         }
@@ -113,7 +126,7 @@ function inject()
     let tp = setInterval(() => { //Stellt sicher das die Elemente erscheinen nach dem man Videos fullscreen gemacht hat
         if (document.getElementsByClassName("GameCanvasContainer-main").length && !document.getElementById("getOverHereButton"))
         {
-            createButtonWithTextbox("getOverHereButton", "Get over here", "targetnameTextbox");
+            createTwoButtonWithTextbox("getOverHereButton","TpToMeButton" , "Get over here","TP to me", "targetnameTextbox");
             createButtonWithTextbox("teleportToButton", "Teleport to", "targetteleportTextbox");
             createButton("zombie","Make me a Robot");
 
@@ -137,7 +150,7 @@ function inject()
 				}else if (walk_i == 2){
 					walk_i = 0;
 				}
-    			}, 5000);
+    			}, 2000);
 		} else {
 			clearInterval(zombie_walk);
 		}
@@ -146,6 +159,10 @@ function inject()
 
             document.getElementById("getOverHereButton").addEventListener("click", function() {
                 getOverHere(document.getElementById("targetnameTextbox").value);
+            }, false);
+
+            document.getElementById("TpToMeButton").addEventListener("click", function() {
+                teleportToMe(document.getElementById("targetnameTextbox").value);
             }, false);
             
             document.getElementById("teleportToButton").addEventListener("click", function() {
@@ -229,6 +246,76 @@ function createButtonWithTextbox(buttonID, buttonText, texboxID)
     
     container.appendChild(textboxWrapper);
     container.appendChild(blueButton);
+
+    document.getElementsByClassName("Layout css-1oteowz")[0].appendChild(container);
+
+}
+
+function createTwoButtonWithTextbox(buttonID1, buttonID2, buttonText1, buttonText2, texboxID)
+{
+
+    let buttonstyle = "display: inline-block; position: relative; box-sizing: border-box; outline: none; \
+    -webkit-box-align: center; align-items: center; -webkit-box-pack: center; justify-content: center; \
+    font-family: inherit; font-weight: 700; transition: background-color 200ms ease 0s, border-color 200ms ease 0s; \
+    cursor: pointer; opacity: 1; overflow: hidden; background-color: rgb(88, 130, 247); \
+    border: 2px solid transparent; width: auto; height: 32px; margin-top: 5px; \
+    border-radius: 16px; font-size: 15px; color: rgb(255, 255, 255) !important; width:100%"
+
+    var blueButton1 = document.createElement("button")
+    blueButton1.setAttribute("id", buttonID1);
+    blueButton1.setAttribute("type", "submit");
+    blueButton1.setAttribute("shape", "rounded");
+    blueButton1.setAttribute("class", "css-7fr7eg");
+    blueButton1.setAttribute("kind", "tertiary");
+    blueButton1.setAttribute("style", buttonstyle);
+
+    var blueButton2 = document.createElement("button")
+    blueButton2.setAttribute("id", buttonID2);
+    blueButton2.setAttribute("type", "submit");
+    blueButton2.setAttribute("shape", "rounded");
+    blueButton2.setAttribute("class", "css-7fr7eg");
+    blueButton2.setAttribute("kind", "tertiary");
+    blueButton2.setAttribute("style", buttonstyle);
+
+    blueButton1.innerHTML = buttonText1;
+    blueButton2.innerHTML = buttonText2;
+    
+    var textboxWrapper = document.createElement("div") ;
+    textboxWrapper.setAttribute("class", "Input light lg");
+    textboxWrapper.setAttribute("style", "height: 30px;");
+    
+    var textbox = document.createElement("input")
+    textbox.setAttribute("id", texboxID)
+    textbox.setAttribute("placeholder", "Target name")
+    textbox.setAttribute("autocomplete", "off")
+    textbox.setAttribute("style", "font-weight: 400;")
+
+    // var select = document.createElement("select");
+    // select.setAttribute("id", texboxID);
+    // select.setAttribute("option","Choose a player");
+    // select.setAttribute("style", "font-weight: 400;");
+
+    // var select = document.getElementById("texboxID");
+    // var options = getPlayers();
+
+    // for(var i = 0; i < options.length; i++) {
+    //     var opt = options[i];
+    //     var el = document.createElement("option");
+    //     el.textContent = opt;
+    //     el.value = opt;
+    //     select.appendChild(el);
+    // }
+    
+    var container = document.createElement("form");
+    container.setAttribute("class", "css-1xt6zos css-ixaah3");
+    container.setAttribute("action", "javascript:void(0);");
+    container.setAttribute("style", "background-color:rgb(40, 45, 78);height: auto !important; min-height: auto; border-radius: 20px; padding: 5px;");  
+    
+    textboxWrapper.appendChild(textbox);
+    
+    container.appendChild(textboxWrapper);
+    container.appendChild(blueButton1);
+    container.appendChild(blueButton2);
 
     document.getElementsByClassName("Layout css-1oteowz")[0].appendChild(container);
 
